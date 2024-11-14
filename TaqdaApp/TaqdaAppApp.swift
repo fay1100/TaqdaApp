84a657c264a9c7d6eca9cfd86a9853ab6760580b
fayzah <ffowz110@hotmail.com>
2024-11-14 21:12:20 +0300

    Initial Commit

---
+++ TaqdaApp/TaqdaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaqdaAppApp.swift
+//  TaqdaApp
+//
+//  Created by Faizah Almalki on 12/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TaqdaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
